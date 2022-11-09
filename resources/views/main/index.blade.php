4e70185106399444216ca2668090ddcf479823cc
David Iglesias <davidiglesiascontact@gmail.com>
2022-11-09 15:29:40 -0500

    commit 2

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,16 @@
+@extends ('layouts.base')
+@section ('content')
+
+
+<div class="contenido"></div>
+    <ul>
+        <li>lorem5</li>
+        <li>lorem5</li>
+        <li>lorem5</li>
+        <li>lorem5</li>
+    </ul>
+</div>
+
+
+
+@endsection
