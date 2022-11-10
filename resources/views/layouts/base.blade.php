e5ed3b069eb3e976c95011c4993963eed49472e8
David Iglesias <davidiglesiascontact@gmail.com>
2022-11-10 06:50:16 -0500

    Starting Index

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,11 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources\css\app.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>base</title>
 </head>
 <body>
     <div class="content">
         @yield('content')
     </div>
+
+    <header>
+            <div class="content">       
+                <a href="#" class="logo" >
+                    <img src="img\yo.jpg" alt="logo">
+                    <h2 class="nombre"> Diever </h2>
+                </a>
+                <nav>
+                    <a href="#" class="a-link"> General </a>
+                    <a href="#" class="a-link"> Proyectos </a>
+                    <a href="#" class="a-link"> Cursos </a>
+                    <a href="#" class="a-link"> Notas </a>
+                </nav>
+            </div>
+    </header>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+
+
 </body>
 </html>
