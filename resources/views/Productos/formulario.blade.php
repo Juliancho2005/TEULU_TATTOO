86797f5c68da18cadf0040d71f44da79d61c129e
Julian Ruiz <juliandavidruiz.m@gmail.com>
2022-11-15 07:58:54 -0500

    q honda muchachos

---
+++ resources/views/Productos/formulario.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido a la creacion de publicaciones</h1>
+<form action="/productos">
+    @csrf
+    <label for="nombre">
+        <span>Ingresa el nombre que va a tener el tatuaje </span>
+    <input type="text" name="nombre" id="nombre">
+    </label>
+    <label for="descripcion">
+        <span>Ingresa una descripcion del tatuaje </span>
+    <input type="text" name="descripcion" id="descripcion">
+    </label>
+    <label for="categoria">
+        <span>Ingresa las categorias que va a tener el tatuaje </span>
+    <input type="Blocktext" name="cat" id="cat">
+    </label>
+    <label for="precio">
+        <span>Ingresa el nombre que va a tener el tatuaje </span>
+    <input type="number" name="precio" id="precio">
+    </label>
+    
+</form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdministradorController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Productocontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/User', UsersController::class);
+Route::resource('/productos',Productocontroller::class);
