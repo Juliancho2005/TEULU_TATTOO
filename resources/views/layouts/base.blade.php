536ed3e50b856c69b562583a0b092cb648b952a4
David Iglesias <davidiglesiascontact@gmail.com>
2022-11-16 22:25:07 -0500

    adelanto

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -32,7 +32,13 @@
         @yield('content')
     </div>
 
-    
+    <footer>
+
+        <div class="pie-pagina">
+            <p>&copy; Teulu-tatto</p>
+        </div>
+
+    </footer>
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
