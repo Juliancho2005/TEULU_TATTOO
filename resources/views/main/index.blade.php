5895e4225185c967b11b1791a688916a24220f34
David Iglesias <davidiglesiascontact@gmail.com>
2022-12-01 06:57:52 -0500

    mejoraindex

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,11 +1,25 @@
 @extends ('layouts.base')
 @section ('content')
 
+    <section id="hero">
+        <h1>Ordena tu tatuaje <br>a domisilio</h1> <br>
+        <button>ver tatuajes</button>
+    </section>
 
-    <div class="hero-slider">
+    <section id="caracteristicas">
+        <div class="container">
+            <ul>
+                <li> Instagram</li>
+                <li> linkedIn</li>
+                <li> Facebook</li>
+                <li> Whatsapp</li>
+            </ul>
+        </div>
+    </section>
+
+    <div id="slider-promotions">
 
         <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-            
             
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -35,30 +49,10 @@
                         </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
                 
-            </div>
+        </div>
             
     </div>
-
-    <section class="chat">
-
-        <button class="click">chat with us!</button>
-
-        <div class="contenido-chat">
-
-        </div>
-
-    </section>
-        
-        
 
 
 @endsection
