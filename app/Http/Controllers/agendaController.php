a14a463ce06db19cd249a129fb26566718022ba6
AlexChannel06 <117471273+AlexChannel06@users.noreply.github.com>
2022-12-15 15:33:26 -0500

    Agenda

---
+++ app/Http/Controllers/agendaController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class agendaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $agendar = new Cita();
+        $agendar -> id_solicitante = $request->get('');
+        $agendar -> fecha = $request->get('');
+        return redirect('');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id_cita)
+    {
+        $searchU = Cita::findOrFail($id_cita);
+        $searchU -> $id_solicitante = $request -> get('');
+        $searchU -> $fecha = $request -> get('');
+        return redirect ('');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $agendaDestroy = Cita::find($id_cita);
+        $agendaDestroy -> delete();
+        return redirect ('');
+    }
+}
